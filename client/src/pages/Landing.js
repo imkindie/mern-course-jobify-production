a8b28454e598f8bb3fe10f7a9b494a8401820bd5
Matthew Kinder <makinder05@sbcglobal.net>
2023-04-27 16:27:31 -0500

    first commit

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,35 @@
+import main from'../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info*/}
+        <div className="info">
+        <h1>job <span>tracking</span> app
+
+        </h1>
+        <p>
+          We are jobify and we are here to help you track your 
+          job applications!
+
+
+        </p>
+        <Link to="/register" className='btn btn-hero'>Login/Register
+
+        </Link>
+      </div>
+      <img src={main} alt='job hunt' className='img main-img'/>
+      </div>
+
+    </Wrapper>
+  )
+}
+
+export default Landing
